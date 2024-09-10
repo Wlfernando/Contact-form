b68d9760b2ae1428dba90e6d99f67b30fa3ba9ce
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-09-10 03:41:17 -0600

    feat: add Radio component

---
+++ src/components/Radio/Radio.tsx
@@ -0,0 +1,52 @@
+import { useRef } from 'react';
+import './Radio.css'
+
+const hasError = 'radio_has-error'
+
+function filterInvalid({ elements }: HTMLFieldSetElement) {
+  return Array.from(elements).some(e => e instanceof HTMLInputElement && !e.validity.valid)
+}
+
+export default function Radio({
+  name,
+  options,
+  onChange,
+  value,
+}:{
+  name: string;
+  options: readonly string[];
+  onChange: Function;
+  value: string;
+}) {
+  const radioRef = useRef<null | HTMLInputElement>(null);
+
+  function checkValidity({currentTarget: target}: React.FocusEvent<HTMLFieldSetElement, Element>) {
+    filterInvalid(target) ?
+      target.classList.add(hasError) :
+      target.classList.remove(hasError)
+  }
+
+  return (
+    <>
+      <fieldset className="radio" name={name} onBlur={checkValidity} onClick={({ currentTarget }) => {!filterInvalid(currentTarget) && currentTarget.classList.remove(hasError)}} >
+        <legend className='radio__name'>{name}</legend>
+        {options.map(option => 
+          <label className='radio__label' key={option} >
+            <input 
+              className='radio__input'
+              type="radio" 
+              value={option} 
+              name={name} 
+              onChange={({ currentTarget: { name, value }}) => onChange(name, value)}
+              required
+              checked={value === option}
+              ref={radioRef}
+            />
+            <span>{option}</span>
+          </label>
+        )}
+        <p className='radio__error'>{radioRef.current?.validationMessage}</p>
+      </fieldset>
+    </>
+  )
+}
