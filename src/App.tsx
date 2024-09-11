3cf1209ee43eb4389e23dd5cf8e2c97c8c9e9747
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-09-11 16:10:30 -0600

    Style: adjust elements

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,10 @@ function App() {
         />
         {notification && <Notification remove={() => setNotification(false)} />}
       </main>
+      <footer className='footer'>
+        <p className='footer__text'>Sound Effect from <a href="https://pixabay.com/sound-effects/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=6346">Pixabay</a></p>
+        <p className='footer__text'>&copy; 2024. Fernando A. Malfavón</p>
+      </footer>
     </>
   )
 }
