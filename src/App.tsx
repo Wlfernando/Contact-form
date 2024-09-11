2368805ca61c6328896bf161fbb09c1bd2f9d896
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-09-11 14:30:47 -0600

    Feat: include the components in the DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
-
+import { useState } from 'react';
 import './App.css'
+import ContactUs from './components/Contact-Us/ContactUs'
+import Notification from './components/Notification/Notification';
 
 function App() {
+  const [reset, setReset] = useState(1);
+  const [notification, setNotification] = useState<boolean>(false);
+
   return (
     <>
-
+      <main className='main'>
+        <ContactUs
+          reset={() => setReset(1 + reset)}
+          key={reset}
+          send={() => setNotification(true)}
+        />
+        {notification && <Notification remove={() => setNotification(false)} />}
+      </main>
     </>
   )
 }
