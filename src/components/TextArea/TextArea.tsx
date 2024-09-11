e5bf58383be85e66ee8955a8768b9359d5b90059
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-09-10 19:13:52 -0600

    feat: add TextArea component

---
+++ src/components/TextArea/TextArea.tsx
@@ -0,0 +1,32 @@
+import { useRef } from "react";
+import './TextArea.css'
+import { checkValidity } from "../../utils/utils";
+
+export default function TextArea({
+  name,
+  value,
+  onChange,
+}: {
+  name: string;
+  value: string;
+  onChange(name: string, value: string): void;
+}) {
+  const textAreaRef = useRef<null | HTMLTextAreaElement>(null);
+
+  return (
+    <>
+      <fieldset name={name} className="text-field" onBlur={checkValidity} >
+        <label className="text-field__name" htmlFor={name} >{name}</label>
+        <textarea 
+          className="text-field__item"
+          name={name}
+          id={name}
+          ref={textAreaRef}
+          required value={value}
+          onChange={({currentTarget: {name, value}}) => onChange(name, value)}
+        ></textarea>
+        <p className="text-field__error">{textAreaRef.current?.validationMessage}</p>
+      </fieldset>
+    </>
+  )
+}
